c6265266703dc0df9a15064723d954b689f9355f
Tolulope Akindeko <tolulopeakindeko@gmail.com>
2025-02-02 11:00:50 +0100

    Pass #1

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./assets/css/**/*.{css,scss}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
