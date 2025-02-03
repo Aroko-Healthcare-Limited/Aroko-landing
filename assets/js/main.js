7db3b729f590cbe9ada317239f5fec554c6b20a5
Tolulope Akindeko <tolulopeakindeko@gmail.com>
2025-02-03 17:07:02 +0100

    Newer Changes to Bootstrapped

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+// const slider = document.getElementById("testimonialSlider");
+// const prevBtn = document.getElementById("prevBtn");
+// const nextBtn = document.getElementById("nextBtn");
+
+// let currentIndex = 0;
+
+// function updateSlider() {
+//     slider.style.transform = `translateX(-${currentIndex * 50}%)`;
+// }
+
+// prevBtn.addEventListener("click", () => {
+//     currentIndex = (currentIndex === 0) ? slider.children.length - 2 : currentIndex - 1;
+//     updateSlider();
+// });
+
+// nextBtn.addEventListener("click", () => {
+//     currentIndex = (currentIndex === slider.children.length - 2) ? 0 : currentIndex + 1;
+//     updateSlider();
+// });
